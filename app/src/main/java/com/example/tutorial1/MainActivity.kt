ed8162189f63b7e3d2661181060dc529a391ff21
hacmachdien <hac.lmh@powermore.vn>
2020-03-30 09:05:11 +0700

    test push 1

--- app/src/main/java/com/example/tutorial1/MainActivity.kt
+++ app/src/main/java/com/example/tutorial1/MainActivity.kt
@@ -17,8 +17,7 @@ class MainActivity : AppCompatActivity() {
             val sendIntent = Intent(this, MessageWindow::class.java)
             sendIntent.putExtra("user_message", userMessage)
             startActivity(sendIntent)
-
-            hahahaa
+            kik
         }
     }
 }
